50a0369161365dcd96947395cb12818e41791067
Raquel Brito <raquelbrito662@gmail.com>
2025-04-21 11:50:26 -0300

    Initial commit

---
+++ src/main/java/com/dev/brito/doevida/DoevidaApplication.java
@@ -0,0 +1,13 @@
+package com.dev.brito.doevida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoevidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoevidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/brito/doevida/DoevidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.brito.doevida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoevidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
